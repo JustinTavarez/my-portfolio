6e7176cb0c11aa5c45e70ced92e29bc9a04082b3
Justin Tavarez <jtavarez@ggc.edu>
2025-12-11 14:47:41 -0500

    Added icon image to page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -2,17 +2,23 @@ const skills = {
   "Languages & Frameworks": [
     "Java",
     "Python",
+    "C#",
     "C++",
     "HTML",
     "CSS",
     "PHP",
     "JavaScript",
-    "React",
+    "TypeScript",
+    "Angular",
+    "Spring Boot",
+    "React.js",
     "Vue.js",
     "Node.js",
   ],
-  "Cloud & Data": ["AWS (Cloud Practitioner)", "S3", "EC2", "RDS basics", "MongoDB", "SQLite"],
-  "Testing & Tools": ["Selenium WebDriver", "JUnit", "Mockito", "Git/GitHub", "VS Code"],
+  "Databases": ["MySQL", "SQLite", "MongoDB", "PostgreSQL", "Firebase", "AWS RDS", "AWS DynamoDB", "AWS Redshift", "AWS Aurora"],
+  "Testing Tools": ["Selenium WebDriver", "JUnit", "Mockito", "ASP.NET Core"],
+  "Certifications": ["AWS: Certified Cloud Practitioner (Click here to view)"],
+
 };
 
 function Skills() {
@@ -29,15 +35,31 @@ function Skills() {
           >
             <h2>{category}</h2>
             <ul>
-              {items.map((item, itemIndex) => (
-                <li 
-                  key={item}
-                  style={{ animationDelay: `${(index * 0.1) + (itemIndex * 0.05) + 0.6}s` }}
-                  className="skill-item"
-                >
-                  {item}
-                </li>
-              ))}
+              {items.map((item, itemIndex) => {
+                const isCertification = category === "Certifications";
+                const certificationUrl = "https://www.credly.com/badges/e9ec424d-a9fc-49cd-bed7-e4965ee4a397/public_url";
+                
+                return (
+                  <li 
+                    key={item}
+                    style={{ animationDelay: `${(index * 0.1) + (itemIndex * 0.05) + 0.6}s` }}
+                    className="skill-item"
+                  >
+                    {isCertification ? (
+                      <a 
+                        href={certificationUrl} 
+                        target="_blank" 
+                        rel="noreferrer"
+                        className="certification-link"
+                      >
+                        {item}
+                      </a>
+                    ) : (
+                      item
+                    )}
+                  </li>
+                );
+              })}
             </ul>
           </div>
         ))}
